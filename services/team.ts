991a188bb4f4fd789e807b032946eaadcc65f9a3
Francisco Brito <francisco.brito@valtech.com>
2023-10-04 11:25:56 +0100

    improve service

--- services/team.ts
+++ services/team.ts
@@ -1,12 +1,15 @@
 import { Developer } from "@/types/developer";
 import { Project } from "@/types/project";
 
-export async function getTeam(): Promise<Developer[]> {
-  const response = await fetch("https://team-api-508w.onrender.com/team");
-  return response.json();
+export async function getTeam() {
+  return fetchFromAPI<Developer[]>("https://team-api-508w.onrender.com/team");
 }
 
-export async function getProjects(): Promise<Project[]> {
-  const response = await fetch("https://team-api-508w.onrender.com/projects");
+export async function getProjects() {
+  return fetchFromAPI<Project[]>("https://team-api-508w.onrender.com/projects");
+}
+
+async function fetchFromAPI<T>(endpoint: string): Promise<T> {
+  const response = await fetch(endpoint);
   return response.json();
 }
